bf8ca4f8e0300d9fda5714977de4f2f909dce7ac
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 10:51:31 +0100

    unset the value of nested array

--- multi_arr.php
+++ multi_arr.php
@@ -12,3 +12,9 @@ echo PHP_EOL, $big['small']['key'], PHP_EOL;
 $big['small']['key'] = 'new_value';
 
 echo PHP_EOL, $big['small']['key'], PHP_EOL;
+
+unset($big['small']['key']);
+
+echo PHP_EOL, $big['small']['key'], PHP_EOL;
+
+var_export($big);
