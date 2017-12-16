d349bf96ce3d0a47df10fbd9e2e2920b9bf64e1b
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 11:00:04 +0100

    added array  and function var_export to show the value

--- multi_arr.php
+++ multi_arr.php
@@ -21,3 +21,15 @@
 
 // var_dump($big);
 
+$cars = [
+    'volvo' => [
+        'xc90', 'xc60', 'v60',
+    ],
+    'skoda' => [
+        'felicia' => [
+            'hatchback', 'kombi', 'pickup',
+        ],
+    ],
+];
+
+var_export($cars);
