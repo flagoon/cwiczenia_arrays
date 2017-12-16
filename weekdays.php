f2ffeaab33c936358b2e30e69bc19b9645b3c0b4
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 10:35:39 +0100

    create array weekdays

---
+++ weekdays.php
@@ -0,0 +1,11 @@
+<?php
+
+$weekdays = [
+    'monday' => 'poniedziałek',
+    'theusday' => 'wtorek',
+    'wednesday' => 'środa',
+    'thursday' => 'czwartek',
+    'friday' => 'piątek',
+    'saturday' => 'sobota',
+    'sunday' => 'niedziela',
+];
